007536bb68e1b6a71240c029204eb10e1dae922d
Alex Cummaudo <alexcu@me.com>
2016-12-10 13:08:08 +1100

    TEST: Modify test script to include Swift compile

--- test/new_lang/exec.sh
+++ test/new_lang/exec.sh
@@ -2,7 +2,7 @@
 
 LANG_TEST=$1
 echo "Generate adapter"
-../../translate --generate clib,pascal,cpp,python -i "test${LANG_TEST}/new_lang.h" --output ../out -l
+../../translate --generate clib,pascal,cpp,python,swift -i "test${LANG_TEST}/new_lang.h" --output ../out -l
 
 echo "Make dynamic library"
 clang++ -shared -g -DBUILDING_SK_LIB -std=c++14 -I "test${LANG_TEST}" "test${LANG_TEST}/new_lang.cpp" ../out/clib/sk_clib.cpp ../out/clib/lib_type_mapper.cpp -I../clib -I../.. -I. -o libSplashKit.dylib -install_name @rpath/libSplashKit.dylib
@@ -50,6 +50,21 @@ echo "Do you wish to run the Python version?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) run_python; break;;
+        No ) break;;
+    esac
+done
+
+function run_swift
+{
+  cp "test${LANG_TEST}/test_program.py" .
+  echo "Running Swift program"
+  swiftc test${LANG_TEST}/TestProgram.swift -L. -lSplashKit -I../out/clib -I../out/swift -Xlinkerl,-rpath,@loader_path -o swift_test${LANG_TEST}
+}
+
+echo "Do you wish to run the Swift version?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) run_swift; break;;
         No ) break;;
     esac
 done
